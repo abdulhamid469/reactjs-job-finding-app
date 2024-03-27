15df76f549c42e9f4f99ee0bf654415494a048e4
Abdul Hamid <hamidkhattak469@gmail.com>
2024-03-27 22:35:15 +0500

    Services section completed

---
+++ src/components/WelcomeText/index.tsx
@@ -0,0 +1,10 @@
+const WelcomeText = () => {
+  return (
+    <div className="w-full md:px-16 px-5 mb-10 flex flex-col md:gap-1 gap-3 text-center">
+      <span className="md:text-3xl text-xl find-blod text-indigo-500">Let's Find you the Perfect Job!</span>
+      <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis incidunt sapiente nemo eius expedita aperiam quos magni odit quibusdam similique</span>
+    </div>
+  )
+}
+
+export default WelcomeText;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,19 @@
+import Filters from "../../components/Filters";
 import Hero from "../../components/Hero";
 import Services from "../../components/Services";
+import WelcomeText from "../../components/WelcomeText";
 
 const Home = () => {
   return (
     <div className="w-full">
       <Hero />
       <Services />
+      <div className="w-full mt-12 mb-12">
+        <WelcomeText />
+        <div className="w-full flex md:flex-row flex-col items-start relative md:px-16 px-5 gap-9">
+          <Filters />
+        </div>
+      </div>
     </div>
   )
 }
