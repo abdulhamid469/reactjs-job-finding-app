ac02fe6e5e7d5e0ad96ac0dbeeb43039a9912fe0
Abdul Hamid <hamidkhattak469@gmail.com>
2024-04-03 23:48:50 +0500

    Working on ListedJob and Save job issue

--- src/components/ListedJobs/index.tsx
+++ src/components/ListedJobs/index.tsx
@@ -1,4 +1,4 @@
-import { BusinessOutline } from "react-ionicons";
+import { BusinessOutline, LocateOutline, NewspaperOutline } from "react-ionicons";
 import { useNavigate } from "react-router";
 
 type Job = {
@@ -33,7 +33,7 @@ const ListedJobs = ({ jobs, setSavedJobs ,savedJobs }: ListedJobsProps) => {
         {
             jobs.map((job) => {
                 return (
-                    <div key={job.id} className="md:w-[49%] w-full bg-white mb-5 rounded-lg flex items-center justify-between md:p-8 px-4 border border-gray-200">
+                    <div key={job.id} className="md:w-[49%] w-full bg-white mb-5 rounded-lg flex items-center justify-between md:p-8 px-4 py-8 border border-gray-200">
                         <div className="flex md:flex-row flex-col md:items-center items-start gap-6">
                             <img src={job.logo} alt={job.title} className="w-[70px]" />
                             <div className="flex flex-col gap-[6px]">
@@ -42,7 +42,19 @@ const ListedJobs = ({ jobs, setSavedJobs ,savedJobs }: ListedJobsProps) => {
                                     <BusinessOutline width={"18px"} height={"18px"} color={"#555"} />
                                     <span className="text-[14px] font-medium text-gray-600">{job.company}</span>
                                 </div>
+                                <div className="flex items-center gap-2">
+                                    <LocateOutline width={"18px"} height={"18px"} color={"#555"} />
+                                    <span className="text-[14px] font-medium text-gray-600">{job.workStatus}</span>
+                                </div>
+                                <div className="flex items-center gap-2">
+                                    <NewspaperOutline width={"18px"} height={"18px"} color={"#555"} />
+                                    <span className="text-[14px] font-medium text-gray-600">{job.contractStatus}</span>
+                                </div>
                             </div>
+                        </div>
+                        <div className="flex flex-col gap-4 self-end">
+                            <button onClick={() => navigate(`/jobs/${job.id}`)} className="text-white font-bold text-lg rounded-md bg-indigo-500 w-40 h-10"> Apply </button>
+                            <div className={`flex items-center gap-2 cursor-pointer rounded-md justify-center py-1 border border-gray-200 ${ savedJobs.some((j) => j === job.id) ? ( <Bookmark color={"#6366fa"} /> ): ( <BookmarkOutline color={"#6366fa"} /> ) }`}></div>
                         </div>
                     </div>
                 )
